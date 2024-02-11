8945cb67e4d5330549abe0295058c796ff993602
Skhrebtan <shrebtan@gmail.com>
2024-02-11 23:55:11 +0200

    12

--- src/components/TabPanel/TaskTab.jsx
+++ src/components/TabPanel/TaskTab.jsx
@@ -1,4 +1,4 @@
-import { Tab, Tabs, TabList,TabPanel} from 'react-tabs';
+import { Tab, gitTabList,TabPanel} from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import { PlusButton } from 'components/PlusButton/PlusButton';
 import { DnDList } from 'components/DnDList/DnDList';
